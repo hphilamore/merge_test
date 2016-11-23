54d622cf67da127aaa99ae289e7b445b509b314f
Hemma <hemma.philamore@bristol.ac.uk>
2016-11-23 15:57:44 +0000

    plot of boat and wind vetors added

--- matlab_sketches/PMW_model/sailboat_model.m
+++ matlab_sketches/PMW_model/sailboat_model.m
@@ -53,37 +53,53 @@ psi_aw = Wp_aw(2,:);
 gs = -p4 * a_aw * sin(delta_s - psi_aw)     
 gr = -sign(psi_aw) * min(abs(pi - abs(psi_aw)), abs(delta_s)) 
 
+figure(1)
+figure(2)
+
 for j = 1:15
 
     [t,z] = ode45(@(t,Z) sailboat(t,Z), tspan, Z_init);
 
-%     plot(t,z(:,1),'r-o',t,z(:,2),'g-d', ...
-%         t,z(:,3),'b-s',t,z(:,4),'m-^',t,z(:,5),'k-*')
+    figure(1)
     plot(t,z(:,1),'r',t,z(:,2),'g', ...
-        t,z(:,3),'b',t,z(:,4),'m',t,z(:,5),'k')
+       t,z(:,3),'b',t,z(:,4),'m',t,z(:,5),'k')
     hold on
+
+%     figure(2)
+%    
+%     plot(t,z(:,1),'r',t,z(:,2),'g', ...
+%         t,z(:,3),'b',t,z(:,4),'m',t,z(:,5),'k')
+%     hold on
     
     % update initial conditions for next iteration: x y theta v w
     for i=1:5
         Z_init(i) = z(end,i);
     end
     
+    figure(2)    
+    DrawRectangle([Z_init(1), Z_init(2), 1.2, 1.5, Z_init(3), 1.2, ...
+                   delta_s, a_tw, psi_tw], 'r-'); 
+    plot(Z_init(1), Z_init(2),'b')
+    hold on
+    
     % update tspan
     tspan = tspan + 1;
     
     % CONTROLLER                    % todo!
     % update sail and rudder angle
-    delta_s = delta_s + 0.05
-    delta_r = delta_r + 0.1
+    %delta_s = abs(delta_s) + 0.05
+    %delta_r = abs(delta_r) + 0.1
     
-    % update wind angle and magnitude
     
+    % update wind angle and magnitude    
     a_tw = abs(a_tw) + 1
-    psi_tw = abs(psi_tw) 
-    
-    if (j > 10)
-        psi_tw = -1 * psi_tw
-    end
+    psi_tw = abs(psi_tw)     
+%     if (j > 9)
+%         %psi_tw = -1 * psi_tw
+%         delta_s = 0
+%         
+%     end
+
     
     % re-compute parameters
     Wc_aw = [(a_tw * cos(psi_tw - theta) - v); ...
@@ -95,14 +111,14 @@ for j = 1:15
     a_aw = Wp_aw(1,:);    
     psi_aw = Wp_aw(2,:);     
     gs = -p4 * a_aw * sin(delta_s - psi_aw);     
-    gr = -sign(psi_aw) * min(abs(pi - abs(psi_aw)), abs(delta_s));  
-    
-    
-        
-    
+    gr = -sign(psi_aw) * min(abs(pi - abs(psi_aw)), abs(delta_s));   
+   
 end
 
+figure(1)
 legend('x', 'y', 'theta', 'v', 'w')
+
+
 
 function dZdt = sailboat(t,Z)
 
@@ -122,6 +138,73 @@ dZdt = [ ...
         (gs * (p6 - p7 * cos(delta_s)) - gr * p8 * cos(delta_r) - p3 ...
            * Z(5) * Z(4)) / p10; ...
         ];
+    
+function h = DrawRectangle(param,style)
+%--------------------------------------------------------------------------
+% H = DRAWRECTANGLE(PARAM,STYLE)
+% This function draws a rectangle with the given parameters:
+% - inputs:
+%          param................... 1x5 array
+%          - param = [a, b, w, h, theta]
+%          - (a,b): the center of the rectangle
+%          - (w,h): width and height of the rectangle > 0
+%          - theta: the rotation angle of the rectangle
+%          style................... string
+%          - plot style string
+% - output:
+%          h....................... plot handler
+%
+%   Usage Examples,
+%
+%   DrawRectangle([0 0 1 1 0]); 
+%   DrawRectangle([-1,2,3,5,3.1415/6],'r-');
+%   h = DrawRectangle([0,1,2,4,3.1415/3],'--');
+%
+%   Rasoul Mojtahedzadeh (mojtahedzadeh _a_ gmail com)
+%   Version 1.00
+%   November, 2011
+%--------------------------------------------------------------------------
+
+if (nargin <1),
+    error('Please see help for INPUT DATA.');
+elseif (nargin==1)
+    style='b-';
+end;
+[m,n] = size(param);
+if(m ~= 1 || n ~= 9)
+    error('param should be an 1x5 array.');
+end
+if(param(3) <=0 || param(4) <=0)
+    error('width and height must be positive values.');
+end
+a = param(1);
+b = param(2);
+w = param(3);
+h = param(4);
+l = param(6);
+theta = param(5);
+delta_s = param(7);
+a_tw = param(8);
+psi_tw = param(9);
+
+X = [-w/2 w/2 w/2 -w/2 -w/2];
+Y = [h/2 h/2 -h/2 -h/2 h/2];
+P = [X;Y];
+ct = cos(theta);
+st = sin(theta);
+R = [ct -st;st ct];
+P = R * P;
+h = plot(P(1,:)+a,P(2,:)+b,style);
+
+S = [a, a + l * cos(theta + delta_s - pi); ...
+     b, b + l * sin(theta + delta_s - pi)];
+f = plot(S(1,:),S(2,:),'b');
+
+W = [a + a_tw * cos(psi_tw), -a_tw * cos(psi_tw); ...
+     b + a_tw * sin(psi_tw), -a_tw * sin(psi_tw)];
+g = quiver(W(1,1),W(2,1),W(1,2),W(2,2),'g');
+axis equal;
+
 
 % convert variable name to string
 % source: http://stackoverflow.com/questions/11453165/matlab-get-string-containing-variable-name
