df3c99e265ce629de46648b2902b758b80a6ded6
Hemma <hemma.philamore@bristol.ac.uk>
2016-11-24 13:11:37 +0000

    new equations included but old equations used to calculate solution. old equation for gr corrected

--- matlab_sketches/PMW_model/sailboat_model.m
+++ matlab_sketches/PMW_model/sailboat_model.m
@@ -2,20 +2,27 @@
 function main
 
 global p1 p2 p3 p4 p5 p6 p7 p8 p9 p10 p11 ...
-       a_tw psi_tw delta_s delta_r Wc_aw Wp_aw gs gr % x y theta v w
+       a_tw psi_tw delta_s delta_r Wc_aw Wp_aw gs gr ...
+       A_sail A_rudder rho_air K_s K_r F_s F_r % x y theta v w
 
 % model 
-p1 = 0.03
-p2 = 40
-p3 = 60
-p4 = 200
-p5 = 1500
-p6 = 0.5
-p7 = 0.5
-p8 = 2
-p9 = 120
-p10 = 400
-p11 = 0.2
+p1 = 0.03;
+p2 = 40;
+p3 = 60;
+p4 = 200;
+p5 = 1500;
+p6 = 0.5;
+p7 = 0.5;
+p8 = 2;
+p9 = 120;
+p10 = 400;
+p11 = 0.2;
+
+A_sail = 0.25;
+A_rudder = 0.05;
+rho_air = 1.225;
+K_s = 0.5 * rho_air * A_sail;
+K_r = 0.5 * rho_air * A_rudder; 
 
 % time 
 tspan = [0 1];
@@ -50,23 +57,29 @@ a_aw = Wp_aw(1,:);
 psi_aw = Wp_aw(2,:);
 
 % force on sail and rudder     
-% gs = -p4 * a_aw * sin(delta_s - psi_aw)     
+% gs = -p4 * a_aw * sin(delta_s - psi_aw) 
 % gr = -sign(psi_aw) * min(abs(pi - abs(psi_aw)), abs(delta_s)) 
+
+% lift and drag coefficients: function of angle of attack
+Cl_s = 1;
+Cd_s = 0.1;
+Cl_r = 1;
+Cd_r = 0.1;
+
+L_s = K_s * a_aw * Cl_s;
+D_s = K_s * a_aw * Cd_s;
+L_r = K_r * v^2 * Cl_r;
+D_r = K_r * v^2 * Cd_r;
+
 gs = -p4 * a_aw * sin(delta_s - psi_aw)     
-gr = -sign(psi_aw) * min(abs(pi - abs(psi_aw)), abs(delta_s)) 
+gr = -p5 * v^2 * sin(delta_r)
+%gr = -sign(psi_aw) * min(abs(pi - abs(psi_aw)), abs(delta_s));
 
-% sail lift coefficient
-% sail drag coefficient
-% rudder lift coefficient
-% rudder drag coefficient
+F_s = [-hypot(L_s, D_s); ...
+       atan2(L_s, D_s)]  
 
-% sail lift
-% sail drag
-% rudder lift
-% rudder drag
-
-% gs (2x1) matrix
-% gr (2x1 matrix)
+F_r = [-hypot(L_r, D_r); ...
+       atan2(L_r, D_r)];    
 
 figure(1)
 figure(2)
@@ -91,7 +104,7 @@ for j = 1:15
         Z_init(i) = z(end,i);
     end
     
-    figure(2)    
+    figure(2) 
     DrawRectangle([Z_init(1), Z_init(2), 1.8, 1.2, Z_init(3), 1.2, ...
                    delta_s, a_tw, psi_tw], 'r-'); 
     plot(Z_init(1), Z_init(2),'b')
@@ -107,8 +120,8 @@ for j = 1:15
     
     
     % update wind angle and magnitude    
-    a_tw = abs(a_tw) + 1
-    psi_tw = abs(psi_tw)     
+    a_tw = abs(a_tw) + 1;
+    psi_tw = abs(psi_tw);   
 %     if (j > 9)
 %         %psi_tw = -1 * psi_tw
 %         delta_s = 0
@@ -118,15 +131,27 @@ for j = 1:15
     
     % re-compute parameters
     Wc_aw = [(a_tw * cos(psi_tw - theta) - v); ...
-         (a_tw * sin(psi_tw - theta))]
+         (a_tw * sin(psi_tw - theta))];
 
     Wp_aw = [hypot(Wc_aw(2,:), Wc_aw(1,:)); ...
-         atan2(Wc_aw(2,:), Wc_aw(1,:))] 
+         atan2(Wc_aw(2,:), Wc_aw(1,:))]; 
      
     a_aw = Wp_aw(1,:);    
-    psi_aw = Wp_aw(2,:);     
-    gs = -p4 * a_aw * sin(delta_s - psi_aw);     
-    gr = -sign(psi_aw) * min(abs(pi - abs(psi_aw)), abs(delta_s));   
+    psi_aw = Wp_aw(2,:); 
+    
+    L_s = K_s * a_aw * Cl_s;
+    D_s = K_s * a_aw * Cd_s;
+    L_r = K_r * Z_init(4)^2 * Cl_r;
+    D_r = K_r * Z_init(4)^2 * Cd_r;
+
+    gs = -p4 * a_aw * sin(delta_s - psi_aw)     
+    gr = -p5 * Z_init(4)^2 * sin(delta_r)
+
+    F_s = [-hypot(L_s, D_s); ...
+           atan2(L_s, D_s)]  
+
+    F_r = [-hypot(L_r, D_r); ...
+           atan2(L_r, D_r)];
    
 end
 
@@ -136,7 +161,8 @@ legend('x', 'y', 'theta', 'v', 'w')
 function dZdt = sailboat(t,Z)
 
 global p1 p2 p3 p4 p5 p6 p7 p8 p9 p10 p11 ...
-       a_tw psi_tw delta_s delta_r Wc_aw Wp_aw gs gr % x y theta v w
+       a_tw psi_tw delta_s delta_r Wc_aw Wp_aw gs gr ...
+       A_sail A_rudder rho_air K_s K_r F_s F_r; % x y theta v w
 
 dZdt = [ ...
         % x
@@ -147,9 +173,12 @@ dZdt = [ ...
         Z(5); ...
         % v
         (gs * sin(delta_s) - gr * p11 * sin(delta_r) - p2 * Z(4)^2) / p9; ...
+%         ( -F_s(1,:) * cos(F_s(2,:)) + F_r(1,:) * cos(F_r(2,:))) / p9; ...
         % w
         (gs * (p6 - p7 * cos(delta_s)) - gr * p8 * cos(delta_r) - p3 ...
-           * Z(5) * Z(4)) / p10; ...
+          * Z(5) * Z(4)) / p10; ...
+%         ( -F_s(1,:) * sin(F_s(2,:)) * (p6 - p7 * cos(delta_s))) ...
+%         - (-F_r(1,:) * sin(F_r(2,:)) * p8 * cos(delta_r)) / p10; ...
         ];
     
 function h = DrawRectangle(param,style)
