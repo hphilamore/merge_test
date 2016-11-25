485a9ee863d8cd5be90249c3772ec271a0aa0521
Hemma <hemma.philamore@bristol.ac.uk>
2016-11-25 12:17:41 +0000

    system of DEs expressed as functions, backup ahead of expressing aero and hydro forces as vectors, not just magnitude

--- matlab_sketches/PMW_model/sailboat_model.m
+++ matlab_sketches/PMW_model/sailboat_model.m
@@ -3,7 +3,7 @@ function main
 
 global p1 p2 p3 p4 p5 p6 p7 p8 p9 p10 p11 ...
        a_tw psi_tw delta_s delta_r Wc_aw Wp_aw gs gr ...
-       F_s F_r 
+       F_s F_r x y theta v w;
 
 % model 
 p1 = 0.03;
@@ -20,7 +20,7 @@ p11 = 0.2;
 % plane area
 A_s = 0.25;
 A_r = 0.05;
-% lift and drag coefficients: function of angle of attack
+% lift and drag coefficients
 Cl_s = 1;
 Cd_s = 0.1;
 Cl_r = 1;
@@ -54,11 +54,10 @@ D_s = aero_force(A_s, a_aw, Cd_s)
 L_r = hydro_force(A_r, v, Cl_r)
 D_r = hydro_force(A_r, v, Cd_r);
 
-F_s = sumAeroVectors(L_s,D_s)  
-F_r = sumAeroVectors(L_r,D_r) 
-
-gs = -p4 * a_aw * sin(delta_s - psi_aw)     
-gr = -p5 * v^2 * sin(delta_r)
+F_s = sumAeroVectors(L_s, D_s)  
+F_r = sumAeroVectors(L_r, D_r) 
+gs = -p4 * a_aw * sin(delta_s - psi_aw)
+gr = -p5 * v^2 * sin(delta_r) 
 %gr = -sign(psi_aw) * min(abs(pi - abs(psi_aw)), abs(delta_s));    
 
 figure(1)
@@ -71,12 +70,7 @@ for j = 1:15
     figure(1)
     plot(t,z(:,1),'r',t,z(:,2),'g', ...
        t,z(:,3),'b',t,z(:,4),'m',t,z(:,5),'k')
-    hold on
-    
-
-%     for i=1:5
-%         Z_init(i) = z(end,i);
-%     end    
+    hold on 
 
     % update initial conditions for next iteration: x y theta v w
     x = z(end,1);
@@ -110,7 +104,6 @@ for j = 1:15
 %         
 %     end
 
-    
     % re-compute parameters
     % apparent wind
     Wc_aw = appWind_c(a_tw, psi_tw, theta, v)
@@ -121,11 +114,10 @@ for j = 1:15
     L_r = hydro_force(A_r, v, Cl_r)
     D_r = hydro_force(A_r, v, Cd_r);
 
-    F_s = sumAeroVectors(L_s,D_s)  
-    F_r = sumAeroVectors(L_r,D_r) 
-
-    gs = -p4 * a_aw * sin(delta_s - psi_aw)     
-    gr = -p5 * v^2 * sin(delta_r)
+    F_s = sumAeroVectors(L_s, D_s)  
+    F_r = sumAeroVectors(L_r, D_r) 
+    gs = -p4 * a_aw * sin(delta_s - psi_aw) 
+    gr = -p5 * v^2 * sin(delta_r)  
     %gr = -sign(psi_aw) * min(abs(pi - abs(psi_aw)), abs(delta_s));    
    
 end
@@ -137,24 +129,22 @@ function dZdt = sailboat(t,Z)
 
 global p1 p2 p3 p4 p5 p6 p7 p8 p9 p10 p11 ...
        a_tw psi_tw delta_s delta_r Wc_aw Wp_aw gs gr ...
-       F_s F_r; 
+       F_s F_r x y theta v w;
+
+model = 'new'
 
 dZdt = [ ...
         % x
-        Z(4) * cos(Z(3)) + p1 * a_tw * cos(psi_tw); ...
+        dxdt(Z(4), Z(3));
         % y
-        Z(4) * sin(Z(3)) + p1 * a_tw * sin(psi_tw); ...
+        dydt(Z(4), Z(3));
         % theta
-        Z(5); ...
+        dthdt(Z(5));
         % v
-%         (gs * sin(delta_s) - gr * p11 * sin(delta_r) - p2 * Z(4)^2) / p9; ...
-        ( -F_s(1,:) * cos(F_s(2,:)) + F_r(1,:) * cos(F_r(2,:))) / p9; ...
+        dvdt(Z(4), model);
         % w
-%         (gs * (p6 - p7 * cos(delta_s)) - gr * p8 * cos(delta_r) - p3 ...
-%           * Z(5) * Z(4)) / p10; ...
-        ( -F_s(1,:) * sin(F_s(2,:)) * (p6 - p7 * cos(delta_s))) ...
-        - (-F_r(1,:) * sin(F_r(2,:)) * p8 * cos(delta_r)) / p10; ...
-        ];       
+        dwdt(Z(4), Z(5), model);
+        ]; 
     
 function h = DrawRectangle(param,style)
 %--------------------------------------------------------------------------
@@ -219,7 +209,6 @@ W = [a - a_tw * cos(psi_tw), a_tw * cos(psi_tw); ...
 g = quiver(W(1,1),W(2,1),W(1,2),W(2,2),'g');
 axis equal;
 
-
 function out = appWind_c(a_tw, psi_tw, theta, v)
 %--------------------------------------------------------------------------
 % Uses polar components of true wind to compute cartesian components of 
@@ -231,7 +220,8 @@ function out = appWind_c(a_tw, psi_tw, theta, v)
 %          v : PMW velocity in direction of heading 
 % - output:
 %          out....................... 2x1 array
-%          out : cartesian components of apparent wind
+%          out : cartesian components of apparent wind relative to PMW
+%          frame
 %--------------------------------------------------------------------------
 out = [(a_tw * cos(psi_tw - theta) - v); ...
        (a_tw * sin(psi_tw - theta))]
@@ -241,12 +231,14 @@ function [out1, out2, out3] = appWind_p(Wc_aw)
 % Uses cartesian components of apparent wind to compute polar components: 
 % - inputs:
 %          Wc_aw....................... 2x1 array
-%          Wc_aw : cartesian components of apparent wind
+%          Wc_aw : cartesian components of apparent wind relative to PMW
+%          frame
 % - output:
 %          out1....................... 2x1 array
-%          out1 : polar components of apparent wind [mag; angle]
+%          out1 : polar components of apparent wind [mag; angle] relative to PMW
+%          frame
 %          out2 : magnitude of apparent wind velocity 
-%          out3 : apparent wind angle relative to north-east-up frame 
+%          out3 : apparent wind angle relative to PMW frame
 %--------------------------------------------------------------------------
 out1 = [hypot(Wc_aw(2,:), Wc_aw(1,:)); ...
        atan2(Wc_aw(2,:), Wc_aw(1,:))];
@@ -293,10 +285,109 @@ function out = sumAeroVectors(lift,drag)
 %          out : Polar components of resultant force [mag; angle]
 %--------------------------------------------------------------------------
 out = [-hypot(lift, drag); ...
-        atan2(lift, drag)]  
+       atan2(lift, drag)]; 
 
+% function out = oldAeroVectors(part)
+% %--------------------------------------------------------------------------
+% % Find the resultant of perpendicular lift or drag forces as polar
+% % components.
+% % - inputs:
+% %          lift : plane area of sail or wing
+% %          drag : fluid velocity
+% % - output:
+% %          out....................... 2x1 array
+% %          out : Polar components of resultant force [mag; angle]
+% %--------------------------------------------------------------------------
+% global p4 p5 a_aw delta_s delta_r psi_aw v
+% 
+% if part == 'new'
+%     %out = -p4 * a_aw * sin(delta_s - psi_aw)
+%     out = -p5 * v^2 * sin(delta_r); 
+%     
+% elseif part == 'old'
+%     out = -p5 * v^2 * sin(delta_r) 
+% end
 
+function out = dxdt(v,theta)
+%--------------------------------------------------------------------------
+% The horizontal velocity of the PMW. 
+% sum of:
+% - x component of velocity (in direction of heading) in  north-east-up frame
+% - horizontal drift due to the true wind velocity
+% - input:
+%          v : velocity
+%          theta : heading angle
+% - output:
+%          out : horizontal velocity
+%--------------------------------------------------------------------------
+global p1 a_tw psi_tw;   
+out =  v * cos(theta) + p1 * a_tw * cos(psi_tw); ...
+    
+function out = dydt(v, theta)
+%--------------------------------------------------------------------------
+% The vertical velocity of the PMW. 
+% sum of:
+% - y component of velocity (in direction of heading) in  north-east-up frame
+% - vertical drift due to the true wind velocity
+% - input:
+%          v : velocity
+%          theta : heading angle
+% - output:
+%          out : horizontal velocity
+%--------------------------------------------------------------------------
+global p1 a_tw psi_tw;   
+out =  v * sin(theta) + p1 * a_tw * sin(psi_tw); ...
+    
+function out = dthdt(w)
+%--------------------------------------------------------------------------
+% The angular velocity of the PMW. 
+%          out : angular velocity
+%--------------------------------------------------------------------------
+out = w; ...
+    
+function out = dvdt(v, version)
+%--------------------------------------------------------------------------
+% The acceleration of the PMW in the direction of the heading (theta). 
+% Component of wind force on sail acting parallel to heading direction  
+% minus the product of:
+% - component of water force on rudder acting parallel to heading direction
+% - rudder break coefficient
+% - input:
+%          v : velocity
+%          version : old model or new model
+% - output:
+%          out : acceleration
+%--------------------------------------------------------------------------
+global p2 p9 p11 gs gr F_s F_r delta_s delta_r;
 
-       
-       
-   
+if version == 'old'
+    out = (gs * sin(delta_s) - gr * p11 * sin(delta_r) - p2 * v^2) / p9; ...
+
+elseif version == 'new'
+    out = ( -F_s(1,:) * cos(F_s(2,:)) + F_r(1,:) * cos(F_r(2,:))) / p9; ...
+end
+
+function out = dwdt(v,w,version)
+%--------------------------------------------------------------------------
+% The acceleration of the PMW in the direction of the heading (theta). 
+% Component of wind force on sail acting parallel to heading direction  
+% minus the product of:
+% - component of water force on rudder acting parallel to heading direction
+% - rudder break coefficient
+% - input:
+%          v : velocity
+%          v : angular velocity
+%          version : old model or new model
+% - output:
+%          out : acceleration
+%--------------------------------------------------------------------------
+global p3 p6 p7 p8 p10 gs gr F_s F_r delta_s delta_r;
+
+if version == 'old'
+    out =  (gs * (p6 - p7 * cos(delta_s)) - gr * p8 * cos(delta_r) - p3 ...
+                * v * w) / p10; ...
+elseif version == 'new'
+    out = ( -F_s(1,:) * sin(F_s(2,:)) * (p6 - p7 * cos(delta_s))) ...
+        - (-F_r(1,:) * sin(F_r(2,:)) * p8 * cos(delta_r)) / p10; ...
+end
+    
